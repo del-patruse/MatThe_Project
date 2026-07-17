a67c765de5a09441d0018d4bfc9c358987fab82c
Teo Tassa <cubeforsapko@gmail.com>
2026-07-17 15:47:28 +0200

    second push idek..

--- Model.py
+++ Model.py
@@ -1,5 +1,6 @@
-Teo Tassa 17.07.2026
+# Teo Tassa 17.07.2026
 import numpy as np
+from mat_params import MatParams
 
 class Model:
     def __init__(self, params: MatParams):
@@ -11,12 +12,12 @@ class Model:
         return 0 #placeholder
     def calculate_G_tensor(self):
         # Calculate the G tensor based on the parameters
-        F = self.params.F
-        G = self.params.G
-        H = self.params.H
-        L = self.params.L
-        M = self.params.M
-        N = self.params.N
+        F = self.params.plastic_params.F
+        G = self.params.plastic_params.G
+        H = self.params.plastic_params.H
+        L = self.params.plastic_params.L
+        M = self.params.plastic_params.M
+        N = self.params.plastic_params.N
 
         #fourth order structural tensor in voigt notation: 
         G_tens = np.array( [[F+G, -F,   -G,   0,    0,    0], 
@@ -24,13 +25,13 @@ class Model:
                             [-G,  -H,   G+H,  0,    0,    0], 
                             [0,   0,    0,    2*L,  0,    0], 
                             [0,   0,    0,    0,    2*M,  0], 
-                            [0,   0     0,    0,    0,    2*N]])#placeholder for now
+                            [0,   0,    0,    0,    0,    2*N]])#placeholder for now
         return G_tens
 
 
 
-class Strain
-     def __init__(self, eps_e, eps_p):
+class Strain:
+    def __init__(self, eps_e, eps_p):
         self.eps_e = eps_e
-        self.eps_p = eps_p 
+        self.eps_p = eps_p
 
---
+++ mat_params.py
@@ -0,0 +1,27 @@
+#Teo Tassqa 16.07.26
+
+import numpy as np
+import torch 
+
+#this would be a struct in c# but dirty python doesn't like that
+class PlasticParams:
+    def __init__(self, F, G, H, L, M, N ): #
+        self.F = F
+        self.G = G
+        self.H = H
+        self.L = L
+        self.M = M
+        self.N = N
+
+class ElasticParams:
+    def __init__(self, E, nu):
+        self.E = E
+        self.nu = nu
+
+
+class MatParams:
+    def __init__(self, elastic_params: ElasticParams, plastic_params: PlasticParams):
+        self.elastic_params = elastic_params
+        self.plastic_params = plastic_params
+
+
